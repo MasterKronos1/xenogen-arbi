6deef00e5cfe73e714efc7fd3e8faeb7a6bc169b
MasterKronos1 <44232109+MasterKronos1@users.noreply.github.com>
2026-03-16 13:30:54 +0200

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@anthropic-ai/sdk']
+  serverExternalPackages: ['groq-sdk'],
 }
-
 module.exports = nextConfig
